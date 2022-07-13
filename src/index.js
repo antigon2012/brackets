ad43f1a63185bf00c4bdbf785295e468dfe907d6
Dzianis Salahub <antigon2012@gmail.com>
2022-07-13 23:10:21 +0300

    feat: implement solution of brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+
+  const BRACKETS_PAIR = {};
+  bracketsConfig.forEach((el) => (BRACKETS_PAIR[el[1]] = el[0]));
+  for (let i = 0; i < str.length; i++) {
+    let curSymbol = str[i];
+    let topEl = stack[stack.length - 1];
+    if (stack.length === 0) {
+      stack.push(curSymbol);
+    } else {
+      if (BRACKETS_PAIR[curSymbol] === topEl) {
+        stack.pop();
+      } else {
+        stack.push(curSymbol);
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
